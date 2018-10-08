990eb8e43ae775ce16c5359e62395c35be24006c
FOREVERIOIOT <694092596@qq.com>
2018-10-08 15:42:34 +0800

    添加自适应字体

--- BKProjectFramework/BKProjectFramework/Common/BKInline.h
+++ BKProjectFramework/BKProjectFramework/Common/BKInline.h
@@ -66,4 +66,108 @@ NS_INLINE CGFloat get_system_tabbar_ui_height() {
     return 49.f;
 }
 
+#pragma mark - 字体
+
+/**
+ 细体
+ */
+UIKIT_STATIC_INLINE UIFont * thin_font(CGFloat pointSize) {
+    if (@available(iOS 8.2, *)) {
+        return [UIFont systemFontOfSize:pointSize weight:UIFontWeightThin];
+    }else {
+        return [UIFont systemFontOfSize:pointSize];
+    }
+}
+
+/**
+ 半细体
+ */
+UIKIT_STATIC_INLINE UIFont * light_font(CGFloat pointSize) {
+    if (@available(iOS 8.2, *)) {
+        return [UIFont systemFontOfSize:pointSize weight:UIFontWeightLight];
+    }else {
+        return [UIFont systemFontOfSize:pointSize];
+    }
+}
+
+/**
+ 正常体
+ */
+UIKIT_STATIC_INLINE UIFont * regular_font(CGFloat pointSize) {
+    if (@available(iOS 8.2, *)) {
+        return [UIFont systemFontOfSize:pointSize weight:UIFontWeightRegular];
+    }else {
+        return [UIFont systemFontOfSize:pointSize];
+    }
+}
+
+/**
+ 半粗体
+ */
+UIKIT_STATIC_INLINE UIFont * medium_font(CGFloat pointSize) {
+    if (@available(iOS 8.2, *)) {
+        return [UIFont systemFontOfSize:pointSize weight:UIFontWeightMedium];
+    }else {
+        return [UIFont systemFontOfSize:pointSize];
+    }
+}
+
+/**
+ 粗体
+ */
+UIKIT_STATIC_INLINE UIFont * semibold_font(CGFloat pointSize) {
+    if (@available(iOS 8.2, *)) {
+        return [UIFont systemFontOfSize:pointSize weight:UIFontWeightSemibold];
+    }else {
+        return [UIFont systemFontOfSize:pointSize];
+    }
+}
+
+#pragma mark - 自适应字体
+
+/**
+ 细体(自适应)
+ */
+UIKIT_STATIC_INLINE UIFont * thin_adapt_font(CGFloat pointSize) {
+    CGFloat multiple = [UIScreen mainScreen].bounds.size.width / 375.0f;
+    CGFloat size = floor(pointSize*multiple);
+    return thin_font(size);
+}
+
+/**
+ 半细体(自适应)
+ */
+UIKIT_STATIC_INLINE UIFont * light_adapt_font(CGFloat pointSize) {
+    CGFloat multiple = [UIScreen mainScreen].bounds.size.width / 375.0f;
+    CGFloat size = floor(pointSize*multiple);
+    return light_font(size);
+}
+
+/**
+ 正常体(自适应)
+ */
+UIKIT_STATIC_INLINE UIFont * regular_adapt_font(CGFloat pointSize) {
+    CGFloat multiple = [UIScreen mainScreen].bounds.size.width / 375.0f;
+    CGFloat size = floor(pointSize*multiple);
+    return regular_font(size);
+}
+
+/**
+ 半粗体(自适应)
+ */
+UIKIT_STATIC_INLINE UIFont * medium_adapt_font(CGFloat pointSize) {
+    CGFloat multiple = [UIScreen mainScreen].bounds.size.width / 375.0f;
+    CGFloat size = floor(pointSize*multiple);
+    return medium_font(size);
+}
+
+/**
+ 粗体(自适应)
+ */
+UIKIT_STATIC_INLINE UIFont * semibold_adapt_font(CGFloat pointSize) {
+    CGFloat multiple = [UIScreen mainScreen].bounds.size.width / 375.0f;
+    CGFloat size = floor(pointSize*multiple);
+    return semibold_font(size);
+}
+
 #endif /* BKInline_h */
